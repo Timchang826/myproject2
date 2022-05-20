ee20f4c4ddc2c97a37e0bb9b4c3e529df2cf9b94
timchang <timchang0001@gmail.com>
2022-05-20 14:50:15 +0800

    first

---
+++ src/main/java/com/matt/ui/Mywin.java
@@ -0,0 +1,12 @@
+package com.matt.ui;
+
+import javax.swing.*;
+
+public class Mywin {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+
+        System.out.println("hi");
+    }
+}
