420cbe2569877362167f29a2623a90a789ff6ca4
timchang <timchang0001@gmail.com>
2022-05-27 16:25:53 +0800

    guess game

--- src/main/java/com/matt/ui/GuessFrame.java
+++ src/main/java/com/matt/ui/GuessFrame.java
@@ -4,22 +4,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
+    JTextField number = new JTextField(8);
+    JButton button = new JButton("guess");
+    Random random = new Random();
+    int x = random.nextInt(100)+1;
+    JLabel label = new JLabel("secret is + "+x);
     public GuessFrame(){
         super();
         setSize(400,200);
         setLocation(200,300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("hi");
-        JLabel label = new JLabel("hello");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                label.setText("hahaha");
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+                if (num>x){
+                   label.setText("smaller");
+                }else if (num<x){
+                    label.setText("bigger");
+                }else {
+                    label.setText("bingo");
+                }
+
             }
         });
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
