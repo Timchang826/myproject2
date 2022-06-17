381ba3622aca71d05d28d726fbd05128d9838856
timchang <timchang0001@gmail.com>
2022-06-17 16:25:42 +0800

    ScoringFile BufferedReader

--- src/main/java/com/tim/score/ScoringFile.java
+++ src/main/java/com/tim/score/ScoringFile.java
@@ -1,17 +1,22 @@
 package com.tim.score;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
-            while (data != -1){
-                System.out.println((char)data);
-                data = fileReader.read();
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            while (line != null){
+     //           System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int eng = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name,eng,math);
+                stu.print();
+                line = reader.readLine();
             }
         }catch (FileNotFoundException e){
             System.out.println("File Not Found");
