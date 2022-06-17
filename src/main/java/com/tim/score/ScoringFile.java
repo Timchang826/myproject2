301fb3e1307757085a2a1ee5f575c237255584cd
timchang <timchang0001@gmail.com>
2022-06-17 16:02:40 +0800

    ScoringFile

---
+++ src/main/java/com/tim/score/ScoringFile.java
@@ -0,0 +1,21 @@
+package com.tim.score;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ScoringFile {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("scores.txt");
+            int data = fileReader.read();
+            System.out.println(data);
+        }catch (FileNotFoundException e){
+            System.out.println("File Not Found");
+        }catch (IOException e){
+            System.out.println("File Read Fall");
+        }
+        System.out.println("testing");
+
+    }
+}
